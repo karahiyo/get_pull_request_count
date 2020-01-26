7273a169df2a10ffe43284e20371637e946fa715
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2020-01-27 00:13:07 +0900

    ..

--- get_pullrequest_count.py
+++ get_pullrequest_count.py
@@ -8,7 +8,6 @@ access_token = config['DEFAULT']['access_token']
 org = config['DEFAULT']['org']
 repo = config['DEFAULT']['repo']
 
-# or using an access token
 g = Github(access_token)
 repo = g.get_repo(f'{org}/{repo}')
 
