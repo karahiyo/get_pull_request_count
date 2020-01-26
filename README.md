431796d9dd653030ada3c4e28a6ad5241aba43bf
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2020-01-27 00:10:45 +0900

    get repo metadata

--- README.md
+++ README.md
@@ -4,6 +4,19 @@ get pullrequest count
 Quick Start
 -----------
 
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+setup
+```
+$ python3 -m venv .venv
+$ source .venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+create config.ini
+see config.ini.dummy
+
+and run script
+
+```
+$ python get_pullrequest_count.py
+```
+
--- get_pullrequest_count.py
+++ get_pullrequest_count.py
@@ -5,9 +5,17 @@ config = configparser.ConfigParser()
 config.read('config.ini', encoding='utf-8')
 
 access_token = config['DEFAULT']['access_token']
+org = config['DEFAULT']['org']
+repo = config['DEFAULT']['repo']
 
 # or using an access token
 g = Github(access_token)
+repo = g.get_repo(f'{org}/{repo}')
 
-for repo in g.get_user().get_repos(type='owner'):
-    print(repo.name)
+pulls = repo.get_pulls(state='closed', sort='created', base='master')
+for pr in pulls[0:10]:
+    print({
+        "created_at": pr.created_at,
+        "user": pr.user
+        })
+
