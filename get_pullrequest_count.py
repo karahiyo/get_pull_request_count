5647dc4fee14dcd7c65c7cf21437e80c1e52266e
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2020-01-27 00:55:48 +0900

    countup

--- get_pullrequest_count.py
+++ get_pullrequest_count.py
@@ -1,5 +1,7 @@
 from github import Github
 import configparser
+from collections import Counter, defaultdict
+
 
 config = configparser.ConfigParser()
 config.read('config.ini', encoding='utf-8')
@@ -11,10 +13,14 @@ repo = config['DEFAULT']['repo']
 g = Github(access_token)
 repo = g.get_repo(f'{org}/{repo}')
 
+c = Counter()
 pulls = repo.get_pulls(state='closed', sort='created', base='master')
-for pr in pulls[0:10]:
+for pr in pulls[0:100]:
     print({
-        "created_at": pr.created_at,
-        "user": pr.user
+        "created_at": pr.created_at.strftime("%Y-%m"),
+        "user": pr.user.login,
         })
+    c.update({f'created_at/{pr.created_at.strftime("%Y-%m")}/user/{pr.user.login}': 1})
+
+print(c)
 
